6c3e7d53f8154415e89897ebb4d990758bf0f373
MKayatuerk <mirkan.kayatuerk@study.hs-dusseldorf.de>
2025-06-25 15:10:43 +0200

    Added Bridge

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const OSDisplay = document.getElementById("OSDisplay");
 
-OSDisplay.innerHTML = platform.os;
+const OSDisplay = document.getElementById("OSDisplay");
+let OS = platform.os;
+
+
+OSDisplay.innerHTML = OS;
+if (platform.os.toString().includes("Windows")) {
+    window.open("https://mirevi.de/","_self");
+}
+else if (platform.os.toString().includes("Android")) {
+    window.open("https://www.hololink.io/","_self");
+}
+else if (platform.os.toString().includes("IOS")
+    && platform.os.toString().includes("Mac")) {
+    window.open("https://scenery.app/","_self");
+}
