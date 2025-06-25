2eab4e8d152c2448904886f551c72a6af6d4ec7a
MKayatuerk <mirkan.kayatuerk@study.hs-dusseldorf.de>
2025-06-25 17:23:58 +0200

    finishing things up

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ let manufacturer = platform.manufacturer
 
 
 OSDisplay.innerHTML = OS;
+OSDisplay.innerHTML = manufacturer;
 if (platform.os.toString().includes("Android")) {
     window.open("https://www.hololink.io/","_self");
 }
 else if (platform.os.toString().includes("iOS") || manufacturer.includes("iP")) {
     window.open("https://scenery.app/","_self");
 }
-else
-    window.open("https://mirevi.de/","_self");
+else {
+    window.open("https://mirevi.de/", "_self");
+}
